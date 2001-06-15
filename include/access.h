1d6785da9456c89c7078898c03b1c160500ff772
Werner Nickel <nickel@mathematik.tu-darmstadt.de>
2001-06-15 14:38:30 +0000

    Revision numbers.  WN

--- include/access.h
+++
@@ -1,7 +0,0 @@
-#if defined (RUN_TIME) 
-   /* variables which determine access functions for words stored in y */
-   unsigned long SC1, SC2, SC3, MASK1, MASK2;
-   SC1 = GSC1;  SC2 = GSC2;  SC3 = SC1 + SC2;
-   MASK1 = (1L << SC1) - 1; MASK2 = (1L << SC2) - 1;
-#endif
-
--- include/times.h
+++
@@ -1,8 +0,0 @@
-/* set time limit when program is used by quotpic */
- 
-#ifndef __TIME_LIMIT__
-#define __TIME_LIMIT__ 
-
-int time_limit; 
-
-#endif
